6dcca6eb5ca41a2bc6989f03973b344393e8b396
Nick <30679225+nickcosmo@users.noreply.github.com>
2021-04-29 21:58:53 +0200

    add mongodb connection

---
+++ back-end/database.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let db;
+
+const mongoConnect = (cb) => {
+    MongoClient.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+    })
+        .then((client) => {
+            db = client.db();
+            cb();
+        })
+        .catch((err) => {
+            console.log(err);
+            // throw err;
+        });
+};
+
+const getDb = () => {
+    if (db) {
+        return db;
+    }
+    console.log("no db found!");
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoConnect = require('./database').mongoConnect;
+const getDb = require('./database').getDb;
+require('dotenv').config()
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    res.json({hello: "hello"});
-})
+// test route!
+app.get('/', (req, res, next) => {
+    res.json({ hello: 'hello' });
+});
 
-app.listen(8080, () => {
-    console.log("connected!");
+// test route!
+app.post('/', (req, res, next) => {
+    const body = req.body;
+    console.log(body);
+    const db = getDb();
+    return db
+        .collection('products')
+        .insertOne({body: "yodle"})
+        .then((data) => {
+            // return res.json(body);
+        })
+        .catch((err) => console.log(err));
+});
+
+mongoConnect(() => {
+    app.listen(8080, () => {
+        console.log('connected!');
+    });
 });
