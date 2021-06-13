6e760eb14d5f42f1744a3b6597bfed574cfc72d3
Nick <30679225+nickcosmo@users.noreply.github.com>
2021-06-13 15:12:38 +0200

    update to footer in nav drawer

--- front-end/src/components/Main.vue
+++ front-end/src/components/Main.vue
@@ -1,7 +1,4 @@
 <template>
-  <!-- <v-system-bar app dark height="75">
-      <v-toolbar-title class="display-1">CHAPPIE</v-toolbar-title>
-    </v-system-bar> -->
   <v-main class="d-flex align-end grey darken-3">
     <AppHeader />
     <NavigationDrawer />
--- front-end/src/components/ui/UserFooter.vue
+++ front-end/src/components/ui/UserFooter.vue
@@ -1,29 +1,33 @@
 <template>
-  <v-footer class="justify-center align-center white--text" height="62">
-    <v-avatar rounded color="red darken-3" size="42" class="mr-5">
-      {{ getUser.name | abbreviation }}
-    </v-avatar>
-    {{ getUser.name }}
-    <v-spacer></v-spacer>
+  <v-list class="grey darken-4">
+    <v-list-item>
+      <v-avatar rounded color="red darken-3" size="42" class="mr-5">
+        {{ getUser.name | abbreviation }}
+      </v-avatar>
+      {{ getUser.name }}
+      <v-spacer></v-spacer>
 
-    <v-menu offset-y left top rounded="lg">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn plain icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-chevron-down</v-icon>
-        </v-btn>
-      </template>
-      <v-list outlined color="grey" class="darken-3">
-        <v-list-item-group>
-          <v-list-item class="mx-2" @click="logout">
-            <v-list-item-title class="pr-4 red--text">
-              <v-icon class="pr-2 mb-1" color="red">mdi-logout-variant</v-icon>
-              Logout
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-menu>
-  </v-footer>
+      <v-menu offset-y left top rounded="lg">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn plain icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list outlined color="grey" class="darken-3">
+          <v-list-item-group>
+            <v-list-item class="mx-2" @click="logout">
+              <v-list-item-title class="pr-4 red--text">
+                <v-icon class="pr-2 mb-1" color="red"
+                  >mdi-logout-variant</v-icon
+                >
+                Logout
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
